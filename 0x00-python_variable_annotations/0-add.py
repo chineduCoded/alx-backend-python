da67d09a05d1b94cf01b0fff55729ca8e440b5a0
chineduCoded <chinedujohn17@gmail.com>
2023-07-06 17:49:59 +0000

    Basic annotations-add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers
+
+    Args:
+        a -> (float): first param
+        b -> (float): second param
+    Return:
+        The sum as float number
+    """
+    return a + b
