7a424119ed395498dbf901743a0813d9904b1777
chineduCoded <chinedujohn17@gmail.com>
2023-07-10 19:40:01 +0000

    Updated

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,7 @@ from heapq import nsmallest
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Executes wait_random n times."""
     delays = []
